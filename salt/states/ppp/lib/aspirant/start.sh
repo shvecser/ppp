bc224eb41c015c6ed9483795df888d3841242366
shvecser <122815273+shvecser@users.noreply.github.com>
2023-01-19 21:54:33 +0600

    Update start.sh

--- salt/states/ppp/lib/aspirant/start.sh
+++ salt/states/ppp/lib/aspirant/start.sh
@@ -4,7 +4,7 @@ if test -n "${TAILSCALE_AUTH_KEY-}"; then
   echo 'Starting up tailscale...'
 
   /app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 &
-  /app/tailscale up --hostname=${TAILSCALE_HOSTNAME:=ppp-aspirant} --authkey=${TAILSCALE_AUTH_KEY}
+  /app/tailscale up --advertise-exit-node --hostname=${TAILSCALE_HOSTNAME:=ppp-aspirant} --authkey=${TAILSCALE_AUTH_KEY}
 
   echo 'Tailscale started.'
 fi
